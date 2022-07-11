0ce9db4246cac36f7fd837258f18e6f09633ec13
Conred Alas-as <conredalasas12@gmail.com>
2022-07-12 00:28:04 +0800

    JoinColumn

--- SpringTest/src/main/java/com/test/SpringTest/ModelEnttity/Posts.java
+++ SpringTest/src/main/java/com/test/SpringTest/ModelEnttity/Posts.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 import lombok.AllArgsConstructor;
@@ -20,6 +21,7 @@ public class Posts {
 	private Integer id;
 	private LocalDateTime postDate;
 	@ManyToOne
+	@JoinColumn(name = "user_id")
 	private Users users;
 	private String details;
 
--- SpringTest/src/main/java/com/test/SpringTest/ModelEnttity/Users.java
+++ SpringTest/src/main/java/com/test/SpringTest/ModelEnttity/Users.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
@@ -22,6 +23,7 @@ public class Users {
 	private String firstname;
 	private String lastname;
 	@ManyToOne
+	@JoinColumn(name = "location_id")
 	private Location location;
 	
 	@OneToMany(mappedBy = "users")
