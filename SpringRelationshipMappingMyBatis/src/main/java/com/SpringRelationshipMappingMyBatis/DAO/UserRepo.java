0e80db8854b6e49d335adc605669ee8ce7540a96
Conred Alas-as <conredalasas12@gmail.com>
2022-07-18 22:31:08 +0800

    changes

--- SpringRelationshipMappingMyBatis/src/main/java/com/SpringRelationshipMappingMyBatis/DAO/UserRepo.java
+++ SpringRelationshipMappingMyBatis/src/main/java/com/SpringRelationshipMappingMyBatis/DAO/UserRepo.java
@@ -33,8 +33,8 @@ public interface UserRepo {
 	
     public  List <Users> findAll();
 
-	@Select("SELECT  id, details as details, post_date as postDate, users_id as users_id FROM posts WHERE id = #{id}")
-	List<Posts> selectUserPosts();
+	@Select("SELECT  id, details as details, post_date as postDate, users_id as users_id FROM posts WHERE users_id = #{id}")
+	List<Posts> selectUserPosts(String id);
 	
 	@Select("SELECT id as id, name as name FROM location WHERE id = #{location_id}")
   Location  selectUserLocation(String location_id);
