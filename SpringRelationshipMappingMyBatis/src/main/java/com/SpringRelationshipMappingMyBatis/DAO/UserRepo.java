cc527d87978dfacc64af380813b2c092b40557be
Conred Alas-as <conredalasas12@gmail.com>
2022-07-18 23:42:44 +0800

    changes

--- SpringRelationshipMappingMyBatis/src/main/java/com/SpringRelationshipMappingMyBatis/DAO/UserRepo.java
+++ SpringRelationshipMappingMyBatis/src/main/java/com/SpringRelationshipMappingMyBatis/DAO/UserRepo.java
@@ -20,6 +20,12 @@ import org.apache.ibatis.annotations.Update;
 @Mapper
 public interface UserRepo {
 	
+	@Select("SELECT  id, details as details, post_date as postDate, users_id as users_id FROM posts WHERE users_id = #{id}")
+	List<Posts> selectUserPosts();
+	
+	@Select("SELECT id as id, name as name FROM location WHERE id = #{location_id}")
+  Location  selectUserLocation();
+	
 	@Select("SELECT id, firstname as firstname, lastname as lastname, location_id as location_id,  email as email FROM users")
 	@Results(value = {
 		@Result(property="id", column="id"),
@@ -33,12 +39,17 @@ public interface UserRepo {
 	
     public  List <Users> findAll();
 
-	@Select("SELECT  id, details as details, post_date as postDate, users_id as users_id FROM posts WHERE users_id = #{id}")
-	List<Posts> selectUserPosts();
-	
-	@Select("SELECT id as id, name as name FROM location WHERE id = #{location_id}")
-  Location  selectUserLocation();
-	
+	@Select("SELECT id, firstname as firstname, lastname as lastname, location_id as location_id,  email as email FROM users  WHERE  id = #{id}")
+	@Results(value = {
+			@Result(property="id", column="id"),
+			@Result(property="firstname", column="firstname"),
+			@Result(property="lastname", column="lastname"),
+			@Result(property="location_id", column="location_id"),
+			@Result(property="email", column="email"),
+			@Result(property="location", column="location_id", javaType=Location.class, one=@One(select="selectUserLocation")),
+			@Result(property="posts", column="id", javaType=List.class, many=@Many(select="selectUserPosts"))
+		})
+	 public Users findById(Integer id);
 
 	@Insert("INSERT INTO users(id,  firstname, lastname,location_id,email) " +
 		      " VALUES (#{id}, #{firstname}, #{lastname}, #{location_id}, #{email} )")
@@ -48,8 +59,7 @@ public interface UserRepo {
 	@Delete("DELETE FROM users WHERE id = #{id}")
 	public int deleteById(Integer id);
 	
-	@Select("SELECT * FROM users  WHERE  id = #{id}")
-	 public Users findById(Integer id);
+
 	
 	 @Update("Update users set "
 	 		+ "id=#{id}, " +
